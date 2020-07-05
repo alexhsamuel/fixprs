cea01ae43d58fd640aab1e76eff8c5630577e9bb
Alex Samuel <alex@alexsamuel.net>
2020-07-05 19:20:18 -0400

    Multiple dtypes for csv load.

--- benchmark/csvfile.py
+++ benchmark/csvfile.py
@@ -11,18 +11,20 @@ from   timing import benchmark
 
 #-------------------------------------------------------------------------------
 
-def load_csv_arr_str(path):
+def load_csv_arr(path, dtype):
     """
     Loads a CSV file to a dict of U arrays using the `csv` module.
     """
     with open(path) as file:
         reader = csv.reader(file)
         names = next(reader)
-        return dict(zip(names, ( np.array(a) for a in zip(*reader) )))
+        arrs = ( np.array(a, dtype=dtype) for a in zip(*reader) )
+        return dict(zip(names, arrs))
 
 
 def run(path):
-    yield "csv", "load_csv_arr_str", benchmark(lambda: load_csv_arr_str(path))
+    yield "csv", "load_csv_arr dtype=object", benchmark(lambda: load_csv_arr(path, object))
+    yield "csv", "load_csv_arr dtype=S20", benchmark(lambda: load_csv_arr(path, "S20"))
     yield "np", "loadtxt str-recarray", benchmark(
         lambda: np.loadtxt(path, skiprows=1, delimiter=",", dtype=np.dtype([ (str(n), str) for n in range(12) ])))
     # FIXME: This returns O arrays of str.
